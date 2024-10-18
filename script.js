8198824014f48abbc006da2049d3c3437f90ea4d
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-10-18 19:51:31 +0100

    disabled scroll when nav is clicked

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
     if (testElement.style.top === "0px") {
       testElement.style.transition = "top 0.4s ease-in-out";
       testElement.style.top = "-100vh";
+      document.body.style.overflow = ""; // Enable scroll
     } else {
       testElement.style.transition = "top 0.2s ease-in-out";
       testElement.style.top = "0px";
+      document.body.style.overflow = "hidden"; // Disable scroll
     }
   });
 });
