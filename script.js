4edca7c1a1f9b65f6d90972d37b0986ce39a9d63
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-11-08 13:42:23 +0000

    finished product page

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggler = document.querySelector(".navbar-toggler");
-  const testElement = document.querySelector(".mobile-menu-bg");
+  const mobileMenu = document.querySelector(".mobile-menu-bg");
 
-  if (toggler && testElement) {
+  if (toggler && mobileMenu) {
     toggler.addEventListener("click", function () {
-      if (testElement.style.top === "0px") {
-        testElement.style.transition = "top 0.4s ease-in-out";
-        testElement.style.top = "-100vh";
+      if (mobileMenu.style.top === "0px") {
+        mobileMenu.style.transition = "top 0.4s ease-in-out";
+        mobileMenu.style.top = "-100vh";
         document.body.style.overflow = ""; // Enable scroll
       } else {
-        testElement.style.transition = "top 0.2s ease-in-out";
-        testElement.style.top = "0px";
+        mobileMenu.style.transition = "top 0.2s ease-in-out";
+        mobileMenu.style.top = "0px";
         document.body.style.overflow = "hidden"; // Disable scroll
       }
     });
   }
-
-  function updateMainImage(src) {
-    document.getElementById("mainImage").src = src;
-  }
-
-  const triggerDropdownElements =
-    document.querySelectorAll(".trigger-dropdown");
-
-  if (triggerDropdownElements.length > 0) {
-    triggerDropdownElements.forEach(function (element) {
-      element.addEventListener("click", function () {
-        var targetId = this.getAttribute("data-target");
-        var targetElement = document.getElementById(targetId);
-        if (targetElement) {
-          targetElement.click();
-        }
-      });
-    });
-  }
 });
+
+function updateMainImage(src) {
+  document.getElementById("mainImage").src = src;
+}
+
+const triggerDropdownElements = document.querySelectorAll(".trigger-dropdown");
+
+if (triggerDropdownElements.length > 0) {
+  triggerDropdownElements.forEach(function (element) {
+    element.addEventListener("click", function () {
+      var targetId = this.getAttribute("data-target");
+      var targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.click();
+      }
+    });
+  });
+}
